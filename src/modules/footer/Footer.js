c0c4cacbd2a243b6a5d80c4be92192d5e88c543c
DanoHrCZ <daniel.hrecin@gmail.com>
2024-04-17 13:08:33 +0200

    last commit

--- src/modules/footer/Footer.js
+++ src/modules/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import  logoNapis  from "../../media/cerne-logo-napis.svg";
+import  logoCode from "../../media/logo_none_background.png";
 import "./Footer.css"
 
 const Footer = () => {
@@ -8,9 +9,13 @@ const Footer = () => {
     <div className="footerWrapper">
     <div className="footerInformations">
         <p>Daniel Hrečin</p>
+        <p>IČO: </p>
         <p>Design | Web Development | Audio Engineering</p>
     </div>
+    <div className="footerImgs">
     <img className="footerLogo" src={logoNapis} alt="logo s nápisem" />
+    <img className="footerLogo" src={logoCode} alt="logo s nápisem" />
+    </div>
     </div>
     </>
 }
