af3c67f650bc72a04492befabe344687c7f3e147
DanoHrCZ <daniel.hrecin@gmail.com>
2024-04-17 14:09:06 +0200

    build

---
+++ build/precache-manifest.02ff3fcda2e3bb5e490e6ca80de92211.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5102711d9d2ad899dc8792fc57a29fa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ffdfbaebdbb6511f0ce",
+    "url": "/static/css/main.523169c0.chunk.css"
+  },
+  {
+    "revision": "1fe65d0130ae82672700",
+    "url": "/static/js/2.00966109.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/static/js/2.00966109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ffdfbaebdbb6511f0ce",
+    "url": "/static/js/main.fbc299db.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  },
+  {
+    "revision": "29b5403f337df56014529e04048cb31a",
+    "url": "/static/media/analyze-on-screen.29b5403f.svg"
+  },
+  {
+    "revision": "127ced6e86264ab2ff0c4f0717c58ebc",
+    "url": "/static/media/calendar.127ced6e.svg"
+  },
+  {
+    "revision": "d0d3650b56efcf3919aaa7066e7f6b7f",
+    "url": "/static/media/cerne-logo-napis.d0d3650b.svg"
+  },
+  {
+    "revision": "8e356c84a3b4ffd197515d54e3848a10",
+    "url": "/static/media/cerne-logo.8e356c84.svg"
+  },
+  {
+    "revision": "5ae1ce7a823b012c86eb6bb0f2bce60d",
+    "url": "/static/media/file-and-folder.5ae1ce7a.svg"
+  },
+  {
+    "revision": "ca6fa6c4fb7c4b9df6a44b4378eeae88",
+    "url": "/static/media/logistics.ca6fa6c4.svg"
+  },
+  {
+    "revision": "15b1724295099b8a118e5f00698e26b4",
+    "url": "/static/media/logo_none_background.15b17242.png"
+  },
+  {
+    "revision": "a08f57ef2e2b3aed9fd033d7bb15807c",
+    "url": "/static/media/mobile-testing-and-group-discussion.a08f57ef.svg"
+  },
+  {
+    "revision": "b83711d76ff4a75b831a6d0216a605d8",
+    "url": "/static/media/working-with-laptop.b83711d7.svg"
+  }
+]);
