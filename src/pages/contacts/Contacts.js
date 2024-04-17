82f9393489944d3f4269ef598f541c196003e107
DanoHrCZ <daniel.hrecin@gmail.com>
2024-04-17 14:05:10 +0200

    last last commit

--- src/pages/contacts/Contacts.js
+++ src/pages/contacts/Contacts.js
@@ -9,7 +9,7 @@ const Contacts = () => {
     return <>
     <Navbar></Navbar>
     <div className="section">
-    <div>
+    <div className="animated-left">
     <p>Pokud máte zájem, napište mi prosím email. Email by měl obsahovat veškeré vaše požadavky a pokud je to potřeba, tak i soubory, se kterýma bych měl pracovat. Potom se domluvíme na veškerých podmínkách a já začnu pracovat na vaší zakázce.</p>
     <p>email: <a className="a" href="mailto:daniel.hrecin@gmail.com">daniel.hrecin@gmail.com</a></p>
     <p>Telefon: <a className="a" href="tel:+420773737985">+420 773 737 985</a></p>
@@ -17,7 +17,7 @@ const Contacts = () => {
     <p>GitHub: <a className="a" href="https://github.com/DanoHrCZ">DanoHrCZ</a></p>
 
     </div>
-    <div><img src={image} alt="" /></div>
+    <div className="animated-right" ><img src={image} alt="" /></div>
     </div>
 
     <Footer></Footer>
